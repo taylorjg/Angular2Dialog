77de19859767b3e854ef56c9899e58ed569a64dc
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-03-31 20:00:06 +0100

    Got basics working in IE too

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,13 @@
     
     var PATHS = {
         libs: [
-            "node_modules/systemjs/dist/system.src.js",
+            "node_modules/es6-shim/es6-shim.js",
+            "node_modules/systemjs/dist/system-polyfills.js",
+            "node_modules/angular2/es6/dev/src/testing/shims_for_IE.js",
             "node_modules/angular2/bundles/angular2-polyfills.js",
-            "node_modules/angular2/bundles/angular2.dev.js",
-            "node_modules/rxjs/bundles/Rx.js"
+            "node_modules/systemjs/dist/system.src.js",
+            "node_modules/rxjs/bundles/Rx.js",
+            "node_modules/angular2/bundles/angular2.dev.js"
         ],
         client: {
             ts: "client/app/**/*.ts",
@@ -25,12 +28,10 @@
     var tsProject = ts.createProject("tsconfig.json");    
     
     gulp.task("clean", function (done) {
-        console.log("Inside clean gulp task");
         del([PATHS.dist], done);
     });
 
     gulp.task("ts", function () {
-        console.log("Inside ts gulp task");
         return gulp
             .src(PATHS.client.ts)
             .pipe(ts(tsProject))
@@ -38,24 +39,20 @@
     });
 
     gulp.task("html", function () {
-        console.log("Inside html gulp task");
         return gulp
             .src(PATHS.client.html)
             .pipe(gulp.dest(PATHS.dist));
     });
 
     gulp.task("libs", function () {
-        console.log("Inside libs gulp task");
         return gulp
             .src(PATHS.libs)
             .pipe(gulp.dest(PATHS.distLibs));
     });
 
     gulp.task("build", ["ts", "html", "libs"], function () {
-        console.log("Inside build gulp task");
     });
 
     gulp.task("default", ["build"], function () {
-        console.log("Inside default gulp task");
     });
 }());
