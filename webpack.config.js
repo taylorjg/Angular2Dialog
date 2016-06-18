c4577aae789fbea25f83927c25de18950feefba1
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-19 00:57:38 +0100

    Sorted out styles.css

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -14,7 +15,8 @@ module.exports = {
     },
     plugins: [
         new CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-        new CopyWebpackPlugin([{ from: './client/index.html', to: 'index.html' }])
+        new HtmlWebpackPlugin({ template: './client/index.html' }),
+        new CopyWebpackPlugin([ { from: './client/styles.css', to: 'styles.css' } ])
     ],
     resolve: {
         extensions: ['', '.ts', '.js']
