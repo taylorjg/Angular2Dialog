21f91b5cc5f7eb43e4b89dfe0a98cc222ae6c2ea
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-19 01:32:05 +0100

    Now running webpack via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,22 @@
-(function() {
-    
-    "use strict";
-    
-    var gulp = require("gulp");
-    var ts = require('gulp-typescript');
-    var del = require('del');
-    
-    var PATHS = {
-        libs: [
-            "node_modules/core-js/client/shim.js",
-            "node_modules/zone.js/dist/zone.js",
-            "node_modules/reflect-metadata/Reflect.js",
-            "node_modules/systemjs/dist/system.src.js"
-        ],
-        client: {
-            ts: "client/app/**/*.ts",
-            html: "client/**/*.{html,css}"
-        },
-        dist: "server/dist",
-        distApp: "server/dist/app",
-        distLibs: "server/dist/libs"
-    };
+'use strict';
 
-    var tsProject = ts.createProject("tsconfig.json");    
-    
-    gulp.task("clean", function (done) {
-        del([PATHS.dist], done);
-    });
+const gulp = require('gulp');
+const del = require('del');
+const webpack = require('webpack');
 
-    gulp.task("ts", function () {
-        return gulp
-            .src(PATHS.client.ts)
-            .pipe(ts(tsProject))
-            .pipe(gulp.dest(PATHS.distApp));
-    });
+gulp.task('clean', function (done) {
+    del(['./server/dist'], done);
+});
 
-    gulp.task("html", function () {
-        return gulp
-            .src(PATHS.client.html)
-            .pipe(gulp.dest(PATHS.dist));
+gulp.task('webpack', function (done) {
+    const config = require('./webpack.config.js');
+    webpack(config, function(err, stats) {
+        done();
     });
+});
 
-    gulp.task("libs", function () {
-        return gulp
-            .src(PATHS.libs)
-            .pipe(gulp.dest(PATHS.distLibs));
-    });
+gulp.task('build', ['clean', 'webpack'], function () {
+});
 
-    gulp.task("build", ["ts", "html", "libs"], function () {
-    });
-
-    gulp.task("default", ["build"], function () {
-    });
-}());
+gulp.task('default', ['build'], function () {
+});
