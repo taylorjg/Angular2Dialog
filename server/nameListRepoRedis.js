a1e4756124336c50e69989ee3d4380dc92a0663c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-01-29 01:43:13 +0000

    Reduced nesting of thens in Redis repo

--- server/nameListRepoRedis.js
+++ server/nameListRepoRedis.js
@@ -8,44 +8,44 @@ const client = redis.createClient();
 client.on('error', err => console.error(`Redis error: ${err}`));
 
 const createItem = details =>
-    client.incrAsync('item:').then(id => {
-        const key = `item:${id}`;
-        return client.zaddAsync('items:', id, key).then(() => {
+    client.incrAsync('item:')
+        .then(id => {
+            const key = `item:${id}`;
+            return client.zaddAsync('items:', id, key).then(() => id);
+        })
+        .then(id => {
+            const key = `item:${id}`;
             const item = Object.assign({}, details, { id });
-            return client.hmsetAsync(`item:${id}`, item).then(() => item);
+            return client.hmsetAsync(key, item).then(() => item);
         });
-    });
 
 const readAllItems = () =>
-    client.zrangeAsync('items:', 0, -1).then(keys =>
-        Promise.map(keys, key => client.hgetallAsync(key)));
+    client.zrangeAsync('items:', 0, -1)
+        .then(keys => Promise.map(keys, key => client.hgetallAsync(key)));
 
 const readItem = id =>
     client.hgetallAsync(`item:${id}`);
 
 const updateItem = (id, details) => {
     const key = `item:${id}`;
-    return client.hgetallAsync(key).then(item => {
-        if (item) {
-            item.firstName = details.firstName;
-            item.lastName = details.lastName;
-            item.email = details.email;
-            return client.hmsetAsync(key, item).then(() => item);
-        }
-        return null;
-    });
+    return client.hgetallAsync(key)
+        .then(item => {
+            if (item) {
+                item.firstName = details.firstName;
+                item.lastName = details.lastName;
+                item.email = details.email;
+                return client.hmsetAsync(key, item).then(() => item);
+            }
+            return null;
+        });
 };
 
 const deleteItem = id => {
     const key = `item:${id}`;
-    return client.hkeysAsync(key).then(fieldNames =>
-        client.hdelAsync(key, fieldNames).then(numFieldsRemoved => {
-            if (numFieldsRemoved === fieldNames.length) {
-                return client.zremAsync('items:', key).then(numMembersRemoved =>
-                    numMembersRemoved === 1);
-            }
-            return false;
-        }));
+    return client.hkeysAsync(key)
+        .then(fieldNames => client.hdelAsync(key, fieldNames))
+        .then(() => client.zremAsync('items:', key))
+        .then(() => true);
 };
 
 module.exports = {
