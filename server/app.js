4a319df6f189aa1d5cfe0840ae34b746f9b94b23
Jon Taylor <jont@cakesolutions.net>
2017-02-01 19:49:12 +0000

    Now using custom middleware to prefer compressed static content

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
-const compression = require('compression');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const nameListService = require('./nameListService');
@@ -14,12 +14,32 @@ nameListServiceRouter.get('/:id', nameListService.readItem);
 nameListServiceRouter.post('/:id', nameListService.updateItem);
 nameListServiceRouter.delete('/:id', nameListService.deleteItem);
 
+const preferCompressedStaticContent = rootFolder => (req, res, next) => {
+    if (req.method === 'GET' && req.get('Accept-Encoding').indexOf('gzip') >= 0) {
+        const normalFilePath = path.join(rootFolder, req.url);
+        const compressedFilePath = normalFilePath + '.gz';
+        fs.stat(normalFilePath, err1 =>
+            fs.stat(compressedFilePath, err2 => {
+                if (!err1 && !err2) {
+                    req.url = req.url + '.gz';
+                    res.set('Content-Encoding', 'gzip');
+                }
+                next();
+            }));
+    }
+    else {
+        next();
+    }
+};
+
+const publicFolder = path.join(__dirname, 'public');
+
 const app = express();
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('combined'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', preferCompressedStaticContent(publicFolder));
+app.use('/', express.static(publicFolder));
 app.use('/api/nameList', nameListServiceRouter);
 
 const port = process.env.PORT || 3000;
