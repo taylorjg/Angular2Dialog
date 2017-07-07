c139832e03d3b464577621e60eed2029c8087576
Jon Taylor <jont@cakesolutions.net>
2017-07-07 11:40:31 +0100

    Added CORS support for the REST API

--- server/app.js
+++ server/app.js
@@ -15,6 +15,10 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use('/', preferCompressedStaticContent(publicFolder));
 app.use('/', express.static(publicFolder));
+app.use('/api/nameList', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
 app.use('/api/nameList', nameListService.router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
